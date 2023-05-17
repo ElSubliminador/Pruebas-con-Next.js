1ecf45d2c7bf51507bd5225f680d7e1505fdae98
Jesus Enriquez Melendez <kattvr.kidd@gmail.com>
2023-05-16 23:53:29 -0600

    Este es el primer commit

---
+++ src/components/button.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Button() {
+  return (
+    <button className='bg-[#c8352b] text-2xl text-white px-56 py-8 rounded-md ring-2 ring-purple-500 ring-offset-4 ring-offset-[#c8352b] '>
+        Boton
+    </button>
+  )
+}
---
+++ src/pages/home.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Button from '@/components/button';
+
+export default function Home() {
+  return (
+    <main>
+        <h1 className='text-center py-6 mt-12 bg-[#c8352b] text-white font-semibold text-5xl border-8 border-[#d5672d]'>
+            Programa de capacitación en el uso de extintores
+        </h1>
+        <div>
+            <Button/>
+        </div>
+    </main>
+  )
+}
